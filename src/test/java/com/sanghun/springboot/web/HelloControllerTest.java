b584483cac1b0a0e89a41a8f7df5ed69178afb74
SangHun <shzero211@naver.com>
2022-07-07 00:00:15 +0900

    Dto +Test,HelloResponseDto+Test 추가

--- src/test/java/com/sanghun/springboot/web/HelloControllerTest.java
+++ src/test/java/com/sanghun/springboot/web/HelloControllerTest.java
@@ -1,14 +1,20 @@
 package com.sanghun.springboot.web;
 
 import junit.framework.TestCase;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+import java.util.regex.Matcher;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -18,8 +24,19 @@ private MockMvc mvc;
 @Test
     public void hello가_리턴된다() throws Exception{
     String hello="hello";
-    mvc.perform(MockMvcRequestBuilders.get("/hello"))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.content().string(hello));
+    mvc.perform(get("/hello"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(hello));
+}
+@Test
+    public void helloDto가_리턴된다()throws Exception{
+    String name="hello";
+    int amount=1000;
+    mvc.perform(get("/hello/dto")
+            .param("name",name)
+            .param("amount",String.valueOf(amount)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name", is(name)))
+             .andExpect(jsonPath("$.amount", is(amount)));
 }
 }
---
+++ src/test/java/com/sanghun/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,19 @@
+package com.sanghun.springboot.web.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트(){
+        //given
+        String name="test";
+        int amount=1000;
+        //when
+        HelloResponseDto dto=new HelloResponseDto(name,amount);
+
+        //then
+        Assertions.assertThat(dto.getName()).isEqualTo(name);
+        Assertions.assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
