60e8475fc9d717f593b6116715394e5d3124c6c5
SangHun <shzero211@naver.com>
2022-07-06 23:13:24 +0900

    lombok depemdency add & HelloController+Test

---
+++ src/test/java/com/sanghun/springboot/web/HelloControllerTest.java
@@ -0,0 +1,25 @@
+package com.sanghun.springboot.web;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest extends TestCase {
+    @Autowired
+private MockMvc mvc;
+@Test
+    public void hello가_리턴된다() throws Exception{
+    String hello="hello";
+    mvc.perform(MockMvcRequestBuilders.get("/hello"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string(hello));
+}
+}
